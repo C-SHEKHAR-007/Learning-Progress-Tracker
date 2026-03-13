475fdf856aa9fd2c0625d567c5e5ae3da1e7c601
Chandra Shekhar Chaudhary <c.shekhar.c101@gmail.com>
2026-03-13 10:00:00 +0530

    export all components from index

---
+++ frontend/src/components/index.js
@@ -0,0 +1,9 @@
+// Central export for all components
+export { default as EmptyState } from './EmptyState';
+export { default as FileUploader } from './FileUploader';
+export { default as Header } from './Header';
+export { default as LearningList } from './LearningList';
+export { default as PDFViewer } from './PDFViewer';
+export { default as Sidebar } from './Sidebar';
+export { default as SubjectManager } from './SubjectManager';
+export { default as VideoPlayer } from './VideoPlayer';
