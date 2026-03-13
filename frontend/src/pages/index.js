1f58824caa39a224ff0bce0bb0c3788c5580005a
Chandra Shekhar Chaudhary <chandrashekharchaudhary10@gmail.com>
2026-03-13 15:30:00 +0530

    add page exports

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+// Central export for all pages
+export { default as Dashboard } from './Dashboard';
+export { default as Library } from './Library';
+export { default as Manage } from './Manage';
+export { default as Player } from './Player';
